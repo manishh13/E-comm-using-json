f2e437185ceaa9dbe1bd59b4b094c84bc6eb9fc1
manishh13 <manishh13>
2024-02-05 21:51:48 +0530

    Add Some more components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
@@ -23,7 +24,7 @@ function App() {
 <Route path="/login" element={< LoginSignup/>}/>
 
     </Routes>
-    
+    <Footer/>
     
     </BrowserRouter>
     </>
